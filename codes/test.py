7a4ebfe43668e5c721b41669967247b9e02ec3eb
amirsoleimani7 <a.soleimani@ec.iut.ac.ir>
2025-03-04 20:27:47 +0330

    class methods

--- codes/test.py
+++ codes/test.py
@@ -1,14 +1,16 @@
-class Item:
-    
+import csv 
+
+
+class Item: 
+
     pay_rate_ = .8     
     all = [] # alist for all the attributes ... 
     
+        
     def __init__(self, name: str, price: float, quantity=0):      
         # run validation to the recived arguments
         assert price >= 0 , f"price [{price}] is not greater than zero!" # the second one is ; f"" is 
-        assert quantity >= 0 , f"quantiy [{quantity}] is not greater than zero!"
-        
-        
+        assert quantity >= 0 , f"quantiy [{quantity}] is not greater than zero!"    
         # Assign to self object
         self.name = name
         self.price = price
@@ -19,29 +21,32 @@ class Item:
             
     def calculate_total_price(self):
         return self.price * self.quantity
-    
+
+
+
+    @classmethod #this is called a decorator ...
+    def instantaite_from_csv(cls): 
+        with open("items.csv" , "r") as f:
+            reader = csv.DictReader(f)
+            items = list(reader)
+            
+        for item in items:
+            print(item)            
+            
+            Item(
+                name = item.get('name') ,
+                price = float(item.get('price')) , 
+                quantity = int(item.get('quantity')) , 
+            )
+                
     def apply_discount(self):
         self.price = self.price * self.pay_rate_
-
+        
     def __repr__(self):
-        return f"[{self.name},{self.price},{self.quantity}]"
-    
-    
+        return f"Item('{self.name}',{self.price},{self.quantity})" # the best practice
 
 
 
-item1 = Item("Phone" , 100 ,1)
-item2 = Item("Laptop", 1000 ,3)
-item3 = Item("Cable" , 10 , 5)
-item4 = Item("Mouse",  50 , 5)
-item5 = Item("Keyboard" , 75 , 5)
-
-
-
+Item.instantaite_from_csv()
 print(f"all the ites are : {Item.all}")
-print(f"for item1 is : {repr(item1)}")
-
-# for i in range(len(Item.all)):
-#     print(f"{i +1} : name is : {Item.all[i].name}")
-
 
